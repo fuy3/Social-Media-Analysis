07ec762d0018aeebd8c1f77ea327681826c8906f
Yiming Fu <yiming.fu3@mail.dcu.ie>
2023-08-22 22:31:05 +0800

    notes_dataset_fixed

--- results/final/notes/01_extract_features.py
+++ results/final/notes/01_extract_features.py
@@ -1,11 +1,11 @@
 # Specify the path to your file
-file_path = 'notes_20_all_comments.txt'
+file_path = 'notes_titles.txt'
 
 # Read the file line by line
 
 with open(file_path, 'r') as file:
 
-    comments = {}
+    comments = 0
 
     for line in file:
         # Parse the JSON object from each line
@@ -14,11 +14,11 @@ with open(file_path, 'r') as file:
         end_index = line.find("', 'type': ")
         content = line[start_index:end_index].strip()   
         
-        if content in comments:
-            print (content)
-        comments[content] = 1
-        #print("https://www.xiaohongshu.com/explore/" + content)  
+        #if content in comments:
+        #comments += int(content)
+        #comments[content] = 1
+        print("https://www.xiaohongshu.com/explore/" + content)  
         #print(content)
         #comments.append(content) 
-    print(len(comments)) 
+    #print(len(comments)) 
 #print(comments)
